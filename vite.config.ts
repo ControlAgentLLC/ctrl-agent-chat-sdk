a249b09090c834d3cdc07cee4ca4d67f3308b33e
Can <can@can.local>
2025-07-11 20:30:45 +0530

    Add Vite configuration and make project buildable with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const entry = resolve(__dirname, "src/app.tsx");
+
+const buildType = process.env.BUILD_TYPE;
+
+const baseConfig = {
+  build: {
+    lib: {
+      entry,
+      name: "MyWidget",
+      formats:
+        buildType === "cdn"
+          ? (["umd"] as ("es" | "cjs" | "umd" | "iife")[])
+          : (["es", "cjs"] as ("es" | "cjs" | "umd" | "iife")[]),
+      fileName: (format: any) => `my-widget.${format}.js`,
+    },
+    outDir: buildType === "cdn" ? "cdn" : "dist",
+    rollupOptions: {
+      external: [],
+    },
+    emptyOutDir: buildType === "cdn" ? false : true,
+  },
+  plugins: buildType === "cdn" ? [] : [dts({ outDir: "dist/types" })],
+};
+
+export default defineConfig(baseConfig);
